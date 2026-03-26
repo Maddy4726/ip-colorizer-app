d59eecc25c2b0cef87cc686026f8c6bcc6bf84e0
Mahadev <maddy@gmail.com>
2026-03-26 22:26:27 +0530

    Added multi file support

--- app.py
+++ app.py
@@ -4,21 +4,48 @@ from openpyxl.styles import PatternFill
 import re
 import tempfile
 
+#Location of the main title
 st.set_page_config(page_title="Excel IP Colorizer", layout="centered")
 
+# Main Title in the page
 st.title("📊 Excel IP Colorizer")
 
 # --- File Upload ---
-excel_file = st.file_uploader("Upload Excel File (.xlsx)", type=["xlsx"])
-ip_file = st.file_uploader("Upload IP List File (.txt)", type=["txt"])
+#excel_file = st.file_uploader("Upload Excel File (.xlsx)", type=["xlsx"])
+#ip_file = st.file_uploader("Upload IP List File (.txt)", type=["txt"])
+excel_file = st.file_uploader(
+    "Upload Excel File",
+    type=["xlsx", "xls", "xlsm"]
+)
+
+ip_file = st.file_uploader(
+    "Upload IP List File",
+    type=["txt", "csv", "xlsx"]
+)
+
 
 st.write("### Uploaded Files")
 if excel_file:
     st.success(f"Excel file: {excel_file.name}")
 if ip_file:
     st.success(f"IP file: {ip_file.name}")
+    st.write(f"Detected IP file type: {ip_file.name}")
 
+def load_ip_list(ip_path):
+    if ip_path.endswith(".txt"):
+        with open(ip_path, 'r') as f:
+            return {line.strip() for line in f if line.strip()}
 
+    elif ip_path.endswith(".csv"):
+        df = pd.read_csv(ip_path)
+        return set(df.iloc[:, 0].astype(str))
+
+    elif ip_path.endswith(".xlsx"):
+        df = pd.read_excel(ip_path)
+        return set(df.iloc[:, 0].astype(str))
+
+    else:
+        raise ValueError("Unsupported IP file format")
 # --- Process Function ---
 def process_excel(excel_path, ip_path, output_path):
     green_fill = PatternFill(start_color='C6EFCE', end_color='C6EFCE', fill_type='solid')
@@ -30,8 +57,8 @@ def process_excel(excel_path, ip_path, output_path):
     )
 
     # Read IP list
-    with open(ip_path, 'r') as f:
-        pinging_ips = {line.strip() for line in f if line.strip()}
+    pinging_ips = load_ip_list(ip_path)
+
 
     workbook = openpyxl.load_workbook(excel_path)
 
